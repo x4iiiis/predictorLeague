663ff21b86e22ffe69fb5985914efede15473b4a
x4iiiis <ryan_o_f@hotmail.co.uk>
2019-07-31 12:10:58 +0100

    Default points

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,6 +66,8 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'correctScores' => 0,
+            'correctOutcomes' => 0,
             'password' => Hash::make($data['password']),
         ]);
     }

852a8100ac9a16401456130cc8612ce712ca66f5
x4iiiis <ryan_o_f@hotmail.co.uk>
2019-08-02 12:27:06 +0100

    aaand again

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,14 +20,14 @@
 
                         @foreach($matches as $match)
                         <div class="row py-2">
-                            <div class="col-4 mx-auto">
+                            <div class="col-3 mx-auto">
                                 <img src="{{ $match->homeEmblem }}" alt="{{ $match->homeTeam }}">
                             </div>
                             <div class="form-group col-6 my-auto mx-auto">
                                 <input class="col-5" name="home{{ $match->id }}"></input>
                                 <input class="col-5" name="away{{ $match->id }}"></input>
                             </div>
-                            <div class="col-4 mx-auto">
+                            <div class="col-3 mx-auto">
                                 <img src="{{ $match->awayEmblem }}" alt="{{ $match->awayTeam }}">
                             </div>
                         </div>

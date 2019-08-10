f2c9ca263bdbf3a35d6d0c6676106b087a1547f0
x4iiiis <ryan_o_f@hotmail.co.uk>
2019-08-10 14:40:55 +0100

    English Clubs

--- database/seeds/TeamsTableSeeder.php
+++ database/seeds/TeamsTableSeeder.php
@@ -125,8 +125,107 @@ class TeamsTableSeeder extends Seeder
             'league' => 'Scottish Championship'
         ]);
 
-        //English Premiership
-        //cba the noo
+        //English Premier League
+        DB::table('teams')->insert([
+            'name' => 'Arsenal',
+            'emblem' => 'img/clubEmblems/Arsenal.png',
+            'league' => 'Premier League'
+        ]);
+        DB::table('teams')->insert([
+            'name' => 'AstonVilla',
+            'emblem' => 'img/clubEmblems/AstonVilla.png',
+            'league' => 'Premier League'
+        ]);
+        DB::table('teams')->insert([
+            'name' => 'Bournemouth',
+            'emblem' => 'img/clubEmblems/Bournemouth.png',
+            'league' => 'Premier League'
+        ]);
+        DB::table('teams')->insert([
+            'name' => 'Burnley',
+            'emblem' => 'img/clubEmblems/Burnley.png',
+            'league' => 'Premier League'
+        ]);
+        DB::table('teams')->insert([
+            'name' => 'Brighton & Hove Albion',
+            'emblem' => 'img/clubEmblems/Brighton.png',
+            'league' => 'Premier League'
+        ]);
+        DB::table('teams')->insert([
+            'name' => 'Chelsea',
+            'emblem' => 'img/clubEmblems/Chelsea.png',
+            'league' => 'Premier League'
+        ]);
+        DB::table('teams')->insert([
+            'name' => 'CrystalPalace',
+            'emblem' => 'img/clubEmblems/CrystalPalace.png',
+            'league' => 'Premier League'
+        ]);
+        DB::table('teams')->insert([
+            'name' => 'Everton',
+            'emblem' => 'img/clubEmblems/Everton.png',
+            'league' => 'Premier League'
+        ]);
+        DB::table('teams')->insert([
+            'name' => 'Leicester',
+            'emblem' => 'img/clubEmblems/Leicester.png',
+            'league' => 'Premier League'
+        ]);
+        DB::table('teams')->insert([
+            'name' => 'Liverpool',
+            'emblem' => 'img/clubEmblems/Liverpool.png',
+            'league' => 'Premier League'
+        ]);
+        DB::table('teams')->insert([
+            'name' => 'Manchester City',
+            'emblem' => 'img/clubEmblems/ManCity.png',
+            'league' => 'Premier League'
+        ]);
+        DB::table('teams')->insert([
+            'name' => 'Manchester United',
+            'emblem' => 'img/clubEmblems/ManUtd.png',
+            'league' => 'Premier League'
+        ]);
+        DB::table('teams')->insert([
+            'name' => 'Newcastle United',
+            'emblem' => 'img/clubEmblems/Newcastle.png',
+            'league' => 'Premier League'
+        ]);
+        DB::table('teams')->insert([
+            'name' => 'Norwich City',
+            'emblem' => 'img/clubEmblems/Norwich.png',
+            'league' => 'Premier League'
+        ]);
+        DB::table('teams')->insert([
+            'name' => 'Sheffield United',
+            'emblem' => 'img/clubEmblems/SheffieldUnited.png',
+            'league' => 'Premier League'
+        ]);
+        DB::table('teams')->insert([
+            'name' => 'Southampton',
+            'emblem' => 'img/clubEmblems/Southampton.png',
+            'league' => 'Premier League'
+        ]);
+        DB::table('teams')->insert([
+            'name' => 'Tottenham Hotspur',
+            'emblem' => 'img/clubEmblems/Spurs.png',
+            'league' => 'Premier League'
+        ]);
+        DB::table('teams')->insert([
+            'name' => 'Watford',
+            'emblem' => 'img/clubEmblems/Watford.png',
+            'league' => 'Premier League'
+        ]);
+        DB::table('teams')->insert([
+            'name' => 'West Ham United',
+            'emblem' => 'img/clubEmblems/WestHam.png',
+            'league' => 'Premier League'
+        ]);
+        DB::table('teams')->insert([
+            'name' => 'Wolverhampton Wanderers',
+            'emblem' => 'img/clubEmblems/Wolves.png',
+            'league' => 'Premier League'
+        ]);
 
     }
 }

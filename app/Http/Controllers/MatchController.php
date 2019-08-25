01c32853f3531b474f9e99f3766b2271e461618d
x4iiiis <ryan_o_f@hotmail.co.uk>
2019-08-25 19:25:48 +0100

    ...

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -21,7 +21,9 @@ class MatchController extends Controller
         if(Auth::user()) {
             $users = User::all();
             $matches = Match::all()->where('kickoff', '>', date('Y-m-d H:i:s'))->sortBy('kickoff');
-            $prevMatches = Match::where('kickoff', '<', date('Y-m-d H:i:s'));
+            $prevMatches = Match::where('kickoff', '<', date('Y-m-d H:i:s'))
+                                    ->where('homegoals', '>=', 0)
+                                    ->simplePaginate(5);;
 
             $predictions = []; 
             

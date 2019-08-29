0b475b00e02dececa4e29dacf7f272e79ce3f895
x4iiiis <ryan_o_f@hotmail.co.uk>
2019-08-29 19:40:42 +0100

    Results Loading

--- resources/js/components/Results.vue
+++ resources/js/components/Results.vue
@@ -1,48 +1,38 @@
 <template>
-    <div v-if="ready" class="card my-2">
+    <div class="card my-2">
             
                 <div class="card-header">Previous Matches</div>
-                    <div class="card-body">
+                    <div v-if="ready" class="card-body">
 
-                    <!--
-                        @isset($prevMatches)
-                            @if(sizeof($prevMatches) != 0)
-                                @foreach($prevMatches as $prevMatch)
-                    -->
+                        <div v-for="match in matches" class="row py-2">
+                            <div class="col-3 mx-auto">
+                                <img :src="match.homeEmblem" :alt="match.homeTeam">
+                            </div>
+                            <div class="col-6 mx-auto my-auto text-center">
+                                <h1 style="display: inline;">{{ match.homegoals }} - </h1>
+                                <h1 style="display: inline;">{{ match.awayGoals }}</h1>
 
-                                <div v-for="match in matches" class="row py-2">
-                                    <div class="col-3 mx-auto">
-                                        <img :src="match.homeEmblem" :alt="match.homeTeam">
-                                    </div>
-                                    <div class="col-6 mx-auto my-auto text-center">
-                                        <h1 style="display: inline;">{{ match.homegoals }} - </h1>
-                                        <h1 style="display: inline;">{{ match.awayGoals }}</h1>
 
-                                        <!-- @isset($predictions) -->
+                                    <table>
+                                    <tr v-for="prediction in predictions[ match.id - 1 ]">
+                                        <td style="text-align:right">
+                                            <small>{{ users[prediction.userID - 1].name }}</small>
+                                        </td>
+                                        <td>
+                                            <small>{{ prediction.homeGoals }} - {{ prediction.awayGoals }}</small>
+                                        </td>
+                                    </tr>
+                                    </table>
 
-                                            <table>
-                                            <!-- @foreach($predictions[  match.id  - 1] as $prediction) -->
-                                            <tr v-for="prediction in predictions[ match.id - 1 ]">
-                                                <td style="text-align:right">
-                                                    <small>{{ users[prediction.userID - 1].name }}</small>
-                                                </td>
-                                                <td>
-                                                    <small>{{ prediction.homeGoals }} - {{ prediction.awayGoals }}</small>
-                                                </td>
-                                            </tr>
-                                            <!-- @endforeach -->
-                                            </table>
-                                        <!-- @endisset -->
-
-                                    </div>
-                                    <div class="col-3 mx-auto">
-                                        <img :src="match.awayEmblem" :alt="match.awayTeam">
-                                    </div>
-                                </div>
-                                <!-- @endforeach
-                            @endif
-                        @endisset -->
+                            </div>
+                            <div class="col-3 mx-auto">
+                                <img :src="match.awayEmblem" :alt="match.awayTeam">
+                            </div>
+                        </div>
                     </div>
+                    <div v-else class="card-body">
+                        <p>Loading . . .</p>
+                    </div>  
                     
                 </div>
         </div>

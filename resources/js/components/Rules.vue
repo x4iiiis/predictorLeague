73606ebb8cc575777652fd7267355b0d9dfa18f9
x4iiiis <ryan_o_f@hotmail.co.uk>
2019-09-25 23:24:59 +0100

    Making Rules a little prettier

--- resources/js/components/Rules.vue
+++ resources/js/components/Rules.vue
@@ -4,11 +4,12 @@
             <div class="card-header">Rules</div>
 
                 <div class="card-body">
-                    <p>Correct Score - 3pts</p>
-                    <p>Wrong Score w/Correct Outcome  - 1pt</p>
-                    <hr>
-                    <p>Cup games will be based on 90 minutes</p>
-
+                    <ul>
+                        <li><p>Correct Score: 3pts</p></li>
+                        <li><p>Wrong Score w/Correct Outcome: 1pt</p></li>
+                        <hr>
+                        <li><p>Cup games will be based on 90 minutes</p></li>
+                    </ul>
                 </div>
             </div>
         </div>

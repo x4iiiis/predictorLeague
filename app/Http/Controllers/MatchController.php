783dd71103f07da48872ac41954ebe007b50e516
x4iiiis <ryan_o_f@hotmail.co.uk>
2019-12-08 01:43:38 +0000

    Wild Punt Up The Park

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -66,13 +66,13 @@ class MatchController extends Controller
             $limit = 10;
             $resultedTotal = sizeof(
                                 Match::orderBy('kickoff', 'desc')
-                                    ->where('kickoff', '<', date('Y-m-d H:i:s'))
+                                    // ->where('kickoff', '<', date('Y-m-d H:i:s'))
                                     ->where('homegoals', '>=', 0)
                                     ->get()
                                 ) + 1;
             //dd(Match::find($counter));
             $prevMatches = Match::orderBy('kickoff', 'desc')
-                                ->where('kickoff', '<', date('Y-m-d H:i:s'))
+                                // ->where('kickoff', '<', date('Y-m-d H:i:s'))
                                 ->where('homegoals', '>=', 0)
                                 ->find(range(
                                     $resultedTotal - (($counter) * $limit) - 1,
